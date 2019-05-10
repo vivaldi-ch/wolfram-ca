38745b46cb5d48055311158f55b23627d6716c5c
vivaldi-ch <vivaldi.ch@gmail.com>
2019-05-10 12:40:06 +0800

    Adds wolfram pattern representation component.

---
+++ src/components/wolfram-rep.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import styles from './wolfram-rep.module.css';
+
+const decimalToBinary = (decimal) => {
+  const binArray = [];
+
+  if (!_.isNumber(decimal)) {
+    return [];
+  }
+
+  for (let i = 0; i < 3; i += 1) {
+    // eslint-disable-next-line no-bitwise
+    binArray[i] = (decimal >> i) & 1; // reverse
+  }
+
+  return binArray.reverse();
+};
+
+const WolframRep = ({ wolframArr }) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: '1em',
+    }}
+  >
+    {
+      wolframArr.map((val, index) => {
+        const valInArray = decimalToBinary(index);
+
+        return (
+          <div>
+            <div className={styles.squareRowWrapper}>
+              {valInArray.map(v => (
+                <div
+                  className={`${styles.square} ${
+                    v === 0 ? styles.white : styles.black
+                  }`}
+                />
+              ))}
+            </div>
+            <div className={styles.squareRowWrapper}>
+              <div
+                className={`${styles.square} ${
+                  val === 0 ? styles.white : styles.black
+                }`}
+              />
+            </div>
+          </div>
+        );
+      })
+    }
+  </div>
+);
+
+WolframRep.propTypes = {
+  wolframArr: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default WolframRep;
