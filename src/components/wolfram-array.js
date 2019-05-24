6d47b2d720fa5aa642219f21913c6d4a0c734cb8
vivaldi-ch <vivaldi.ch@gmail.com>
2019-05-24 17:42:40 +0800

    Fixes error when number is less than 0 or more than 255.

--- src/components/wolfram-array.js
+++ src/components/wolfram-array.js
@@ -8,35 +8,50 @@ import styles from './wolfram-ca.module.css';
 const CELL_SIDE = 15;
 
 class WolframArray extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { value } = this.props;
+    const isNumberValid = value > 0 && value < 256;
+
+    this.state = {
+      isNumberValid,
+    };
+  }
+
   componentDidMount() {
     this.updateCanvas();
   }
 
   updateCanvas() {
     const { value, width, height } = this.props;
+    const { isNumberValid } = this.state;
+
+    if (!isNumberValid) return;
+
     const ctx = this.canvas.getContext('2d');
 
     getWolframCAArray(value, width, height).forEach((object, index) => {
       object.forEach((o, objectIndex) => {
         if (o === 1) {
-          ctx.fillRect(
-            objectIndex * CELL_SIDE,
-            index * CELL_SIDE,
-            CELL_SIDE,
-            CELL_SIDE,
-          );
+          ctx.fillRect(objectIndex * CELL_SIDE, index * CELL_SIDE, CELL_SIDE, CELL_SIDE);
         }
       });
     });
   }
 
   render() {
-    const { value } = this.props;
-    const isNumberValid = value > 0 && value < 256;
+    const { isNumberValid } = this.state;
 
     const wolframHTML = isNumberValid ? (
       <div className={styles.table}>
-        <canvas ref={(canvas) => { this.canvas = canvas; }} width={765} height={7500} />
+        <canvas
+          ref={(canvas) => {
+            this.canvas = canvas;
+          }}
+          width={765}
+          height={7500}
+        />
       </div>
     ) : (
       <h4 className={styles.notFound}>Invalid number.</h4>
